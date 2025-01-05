e307e38bd0e2e188d1583dbecd03c745d6540046
Nocebo-Effect/babmv <107328572+TheNoceboEffect@users.noreply.github.com>
2025-01-05 05:24:52 +0000

    commit

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -1,4 +1,6 @@
-const audioEngine = Scratch.vm.runtime.audioEngine;
+const Tone = require('tone'); // Import Tone.js for sound synthesis
+let audioURL = 'https://thenoceboeffect.github.io/sounds/a.ogg'; // This will store the loaded URL
+let player;
 const BlockType = require('../../extension-support/block-type');
 const ArgumentType = require('../../extension-support/argument-type');
 const TargetType = require('../../extension-support/target-type');
@@ -6,7 +8,7 @@ const TargetType = require('../../extension-support/target-type');
 class Scratch3YourExtension {
 
     constructor (runtime) {
-        runtime.audioEngine
+      this.synth = new Tone.Synth().toDestination(); // Setup Tone.js synth
     }
 
     /**
@@ -30,217 +32,65 @@ class Scratch3YourExtension {
 
             // your Scratch blocks
             blocks: [
-                {
-                    // name of the function where your block code lives
-                    opcode: 'speak',
+              {
+                  opcode: 'loadSound',
+                  blockType: Scratch.BlockType.COMMAND,
+                  text: 'load sound from [URL]',
+                  arguments: {
+                      URL: {
+                          type: Scratch.ArgumentType.STRING,
+                          defaultValue: 'https://thenoceboeffect.github.io/sounds/a.ogg'
+                      }
+                  }
+              },
+              {
+                  opcode: 'setPitch',
+                  blockType: Scratch.BlockType.COMMAND,
+                  text: 'set pitch rate to [RATE]',
+                  arguments: {
+                      RATE: {
+                          type: Scratch.ArgumentType.NUMBER,
+                          defaultValue: 1.0
+                      }
+                  }
+              },
+              {
+                  opcode: 'playSound',
+                  blockType: Scratch.BlockType.COMMAND,
+                  text: 'play sound'
+              }
+           ],
+           menus: {},
 
-                    // type of block - choose from:
-                    //   BlockType.REPORTER - returns a value, like "direction"
-                    //   BlockType.BOOLEAN - same as REPORTER but returns a true/false value
-                    //   BlockType.COMMAND - a normal command block, like "move {} steps"
-                    //   BlockType.HAT - starts a stack if its value changes from false to true ("edge triggered")
-                    blockType: BlockType.REPORTER,
-
-                    // label to display on the block
-                    text: 'speak [url]',
-
-                    // true if this block should end a stack
-                    terminal: false,
-
-                    // where this block should be available for code - choose from:
-                    //   TargetType.SPRITE - for code in sprites
-                    //   TargetType.STAGE  - for code on the stage / backdrop
-                    // remove one of these if this block doesn't apply to both
-                    filter: [ TargetType.SPRITE, TargetType.STAGE ],
-
-                    // arguments used in the block
-                    arguments: {
-                        url: {
-                            // default value before the user sets something
-                            defaultValue: 'https://extensions.turbowarp.org/meow.mp3',
-
-                            // type/shape of the parameter - choose from:
-                            //     ArgumentType.ANGLE - numeric value with an angle picker
-                            //     ArgumentType.BOOLEAN - true/false value
-                            //     ArgumentType.COLOR - numeric value with a colour picker
-                            //     ArgumentType.NUMBER - numeric value
-                            //     ArgumentType.STRING - text value
-                            //     ArgumentType.NOTE - midi music value with a piano picker
-                            type: ArgumentType.STRING
-                        }
-                    }
-                }
-            ]
+           loadSound({ URL }) {
+              loadSound(URL);
+           },
+           setPitch({ RATE }) {
+              setPitch(RATE);
+           },
+           playSound() {
+              playSound();
+           }
         };
-    }
-
-
-    /**
-     * implementation of the block with the opcode that matches this name
-     *  this will be called when the block is used
-     */
-    speak ({url}) {
-        // example implementation to return a string
-        const audioEngine = Scratch.vm.runtime.audioEngine;
-
-  /**
-   * This method assumes that the caller has already requested permission to fetch the URL.
-   * @param {string} url
-   * @returns {Promise<ArrayBuffer>}
-   */
-  const fetchAsArrayBufferWithTimeout = (url) =>
-    new Promise((resolve, reject) => {
-      // Permission is checked in playSound()
-      // eslint-disable-next-line no-restricted-syntax
-      const xhr = new XMLHttpRequest();
-      let timeout = setTimeout(() => {
-        xhr.abort();
-        reject(new Error("Timed out"));
-      }, 5000);
-      xhr.onload = () => {
-        clearTimeout(timeout);
-        if (xhr.status === 200) {
-          resolve(xhr.response);
-        } else {
-          reject(new Error(`HTTP error ${xhr.status} while fetching ${url}`));
-        }
-      };
-      xhr.onerror = () => {
-        clearTimeout(timeout);
-        reject(new Error(`Failed to request ${url}`));
-      };
-      xhr.responseType = "arraybuffer";
-      xhr.open("GET", url);
-      xhr.send();
-    });
-
-  /**
-   * @type {Map<string, {sound: AudioEngine.SoundPlayer | null, error: unknown}>}
-   */
-  const soundPlayerCache = new Map();
-
-  /**
-   * @param {string} url
-   * @returns {Promise<AudioEngine.SoundPlayer>}
-   */
-  const decodeSoundPlayer = async (url) => {
-    const cached = soundPlayerCache.get(url);
-    if (cached) {
-      if (cached.sound) {
-        return cached.sound;
-      }
-      throw cached.error;
-    }
-
-    try {
-      const arrayBuffer = await fetchAsArrayBufferWithTimeout(url);
-      const soundPlayer = await audioEngine.decodeSoundPlayer({
-        data: {
-          buffer: arrayBuffer,
-        },
-      });
-      soundPlayerCache.set(url, {
-        sound: soundPlayer,
-        error: null,
-      });
-      return soundPlayer;
-    } catch (e) {
-      soundPlayerCache.set(url, {
-        sound: null,
-        error: e,
-      });
-      throw e;
-    }
-  };
-
-  /**
-   * @param {string} url
-   * @param {VM.Target} target
-   * @returns {Promise<boolean>} true if the sound could be played, false if the sound could not be decoded
-   */
-  const playWithAudioEngine = async (url, target) => {
-    const soundBank = target.sprite.soundBank;
-
-    /** @type {AudioEngine.SoundPlayer} */
-    let soundPlayer;
-    try {
-      const originalSoundPlayer = await decodeSoundPlayer(url);
-      soundPlayer = originalSoundPlayer.take();
-    } catch (e) {
-      console.warn(
-        "Could not fetch audio; falling back to primitive approach",
-        e
-      );
-      return false;
-    }
-
-    soundBank.addSoundPlayer(soundPlayer);
-    await soundBank.playSound(target, soundPlayer.id);
-
-    delete soundBank.soundPlayers[soundPlayer.id];
-    soundBank.playerTargets.delete(soundPlayer.id);
-    soundBank.soundEffects.delete(soundPlayer.id);
-
-    return true;
-  };
-
-  /**
-   * This method assumes that the caller has already requested permission to fetch the URL.
-   * @param {string} url
-   * @param {VM.Target} target
-   * @returns {Promise<void>}
-   */
-  const playWithAudioElement = (url, target) =>
-    new Promise((resolve, reject) => {
-      // Unfortunately, we can't play all sounds with the audio engine.
-      // For these sounds, fall back to a primitive <audio>-based solution that will work for all
-      // sounds, even those without CORS.
-      // Permission is checked in playSound()
-      // eslint-disable-next-line no-restricted-syntax
-      const mediaElement = new Audio(url);
-
-      // Make a minimal effort to simulate Scratch's sound effects.
-      // We can get pretty close for volumes <100%.
-      // playbackRate does not have enough range for simulating pitch.
-      // There is no way for us to pan left or right.
-      mediaElement.volume = target.volume / 100;
-
-      mediaElement.onended = () => {
-        resolve();
-      };
-      mediaElement
-        .play()
-        .then(() => {
-          // Wait for onended
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    });
-
-  /**
-   * @param {string} url
-   * @param {VM.Target} target
-   * @returns {Promise<void>}
-   */
-  const playSound = async (url, target) => {
-    try {
-      if (!(await Scratch.canFetch(url))) {
-        throw new Error(`Permission to fetch ${url} denied`);
       }
 
-      const success = await playWithAudioEngine(url, target);
-      if (!success) {
-        return await playWithAudioElement(url, target);
+loadSound({url}) {
+audioURL = url; // Store the URL in the variable
+  player = new Tone.Player(audioURL).toDestination();
+  player.autostart = false;
+}
+
+setPitch({rate}) {
+   if (player) {
+       player.playbackRate = rate; // Example: 1.0 is normal, 2.0 doubles pitch, 0.5 halves it
       }
-    } catch (e) {
-      console.warn(`All attempts to play ${url} failed`, e);
-      play({ path }, util); {
-        playSound(path, util.target);
-                }
-            }
-        };
+  }
+
+
+playSound () {
+   if (player) {
+       player.start();
     }
 }
 
-module.exports = Scratch3YourExtension;
+module.exports = Scratch3YourExtension; // Export the extension for unsandboxed usage
