3a818aa3981019ac2dde2871cb2338a47aef638e
Nocebo-Effect/babmv <107328572+TheNoceboEffect@users.noreply.github.com>
2025-01-08 18:48:24 -0800

    Add files via upload

---
+++ BuggedCodePitchandSoundPlayer.js
@@ -0,0 +1,102 @@
+// Original Code
+
+const Tone = require('tone'); // Import Tone.js for sound synthesis
+let audioURL = 'https://thenoceboeffect.github.io/sounds/a.ogg'; // This will store the loaded URL
+let player;
+const BlockType = require('../../extension-support/block-type');
+const ArgumentType = require('../../extension-support/argument-type');
+const TargetType = require('../../extension-support/target-type');
+
+class Scratch3YourExtension {
+
+    constructor (runtime) {
+      this.runtime = runtime;
+
+    this.player = new Tone.Player().toDestination();
+    }
+
+    /**
+     * Returns the metadata about your extension.
+     */
+    getInfo () {
+        return {
+            // unique ID for your extension
+            id: 'yourScratchExtension',
+
+            // name that will be displayed in the Scratch UI
+            name: 'ScratchSpeech',
+
+            // colours to use for your extension blocks
+            color1: '#000099',
+            color2: '#660066',
+
+            // icons to display
+            blockIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
+            menuIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
+
+            // your Scratch blocks
+            blocks: [
+              {
+                  opcode: 'loadSound',
+                  blockType: Scratch.BlockType.COMMAND,
+                  text: 'load sound from [URL]',
+                  arguments: {
+                      URL: {
+                          type: Scratch.ArgumentType.STRING,
+                          defaultValue: 'https://thenoceboeffect.github.io/sounds/a.ogg'
+                      }
+                  }
+              },
+              {
+                  opcode: 'setPitch',
+                  blockType: Scratch.BlockType.COMMAND,
+                  text: 'set pitch rate to [RATE]',
+                  arguments: {
+                      RATE: {
+                          type: Scratch.ArgumentType.NUMBER,
+                          defaultValue: 1.0
+                      }
+                  }
+              },
+              {
+                  opcode: 'playSound',
+                  blockType: Scratch.BlockType.COMMAND,
+                  text: 'play sound'
+              }
+           ],
+           menus: {},
+
+           loadSound({ URL }) {
+              loadSound(URL);
+           },
+           setPitch({ RATE }) {
+              setPitch(RATE);
+           },
+           playSound() {
+              playSound();
+           },
+        }
+      }
+
+loadSound({ url }) {
+audioURL = url; // Store the URL in the variable
+  player = new Tone.Player(audioURL).toDestination();
+  Tone.loaded().then(() => {
+    player.autostart = false;
+    player.start();
+});
+
+setPitch({ rate }) {
+   if (player) {
+       player.playbackRate = rate; // Example: 1.0 is normal, 2.0 doubles pitch, 0.5 halves it
+      }
+  }
+
+
+playSound () {
+   if (player) {
+       player.start();
+    }
+}
+
+module.exports = Scratch3YourExtension; // Export the extension for unsandboxed usage
