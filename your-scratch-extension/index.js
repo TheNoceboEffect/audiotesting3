33c081fc6d221431fb2121cc09792c54469c5e3d
has315 <hasllcdev@gmail.com>
2025-01-11 15:24:26 +0100

    revert extension to previous version

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -1,112 +1,36 @@
-// const Tone = require('tone'); // Import Tone.js for sound synthesis
-// let player;
-// let url = 'https://thenoceboeffect.github.io/sounds/a.ogg'
-// const BlockType = require('../../extension-support/block-type');
-// const ArgumentType = require('../../extension-support/argument-type');
-// const TargetType = require('../../extension-support/target-type');
-
-// class Scratch3YourExtension {
-
-//     constructor (runtime) {
-//       this.runtime = runtime;
-//       this.player = new Tone.Player().toDestination();
-//     }
-
-// //Define Helper Methods
-// playSound({ url }) {
-//     this.player.load(url).then(() => {
-//       this.player.start();
-//     }).catch(error => {
-//       console.error('Error loading sound:', error);
-//     });
-// }
-
-//     /**
-//      * Returns the metadata about your extension.
-//      */
-//     getInfo () {
-//         return {
-//             // unique ID for your extension
-//             id: 'yourScratchExtension',
-
-//             // name that will be displayed in the Scratch UI
-//             name: 'ScratchSpeech',
-
-//             // colours to use for your extension blocks
-//             color1: '#000099',
-//             color2: '#660066',
-
-//             // icons to display
-//             blockIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
-//             menuIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
-
-//             // your Scratch blocks
-//             blocks: [
-//               {
-//                   opcode: 'playSound',
-//                   blockType: Scratch.BlockType.COMMAND,
-//                   text: 'load sound from [url]',
-//                   arguments: {
-//                       url: {
-//                           type: Scratch.ArgumentType.STRING,
-//                           defaultValue: 'https://thenoceboeffect.github.io/sounds/a.ogg'
-//                       }
-//                   }
-//               },
-//               {
-//                   opcode: 'setPitch',
-//                   blockType: Scratch.BlockType.COMMAND,
-//                   text: 'set pitch rate to [RATE]',
-//                   arguments: {
-//                       RATE: {
-//                           type: Scratch.ArgumentType.NUMBER,
-//                           defaultValue: 1.0
-//                       }
-//                   }
-//               }
-//            ],
-//            menus: {},
-
-//            playSound({ URL }) {
-//               loadSound(URL);
-//            },
-//            setPitch({ RATE }) {
-//               setPitch(RATE);
-//            },
-//         }
-//       }
-// }
-
-// setPitch({ rate });
-//    if (player) {
-//        player.playbackRate = rate; // Example: 1.0 is normal, 2.0 doubles pitch, 0.5 halves it
-//     }
-
-// module.exports = Scratch3YourExtension; // Export the extension for unsandboxed usage
-
-
-
-
+const Tone = require('tone'); // Import Tone.js for sound synthesis
+let player;
+let url = 'https://thenoceboeffect.github.io/sounds/a.ogg'
 const BlockType = require('../../extension-support/block-type');
 const ArgumentType = require('../../extension-support/argument-type');
 const TargetType = require('../../extension-support/target-type');
 
 class Scratch3YourExtension {
 
-    constructor (runtime) {
-        // put any setup for your extension here
+    constructor(runtime) {
+        this.runtime = runtime;
+        this.player = new Tone.Player().toDestination();
+    }
+
+    //Define Helper Methods
+    playSound({ url }) {
+        this.player.load(url).then(() => {
+            this.player.start();
+        }).catch(error => {
+            console.error('Error loading sound:', error);
+        });
     }
 
     /**
      * Returns the metadata about your extension.
      */
-    getInfo () {
+    getInfo() {
         return {
             // unique ID for your extension
             id: 'yourScratchExtension',
 
             // name that will be displayed in the Scratch UI
-            name: 'Demo',
+            name: 'ScratchSpeech',
 
             // colours to use for your extension blocks
             color1: '#000099',
@@ -119,71 +43,44 @@ class Scratch3YourExtension {
             // your Scratch blocks
             blocks: [
                 {
-                    // name of the function where your block code lives
-                    opcode: 'myFirstBlock',
-
-                    // type of block - choose from:
-                    //   BlockType.REPORTER - returns a value, like "direction"
-                    //   BlockType.BOOLEAN - same as REPORTER but returns a true/false value
-                    //   BlockType.COMMAND - a normal command block, like "move {} steps"
-                    //   BlockType.HAT - starts a stack if its value changes from false to true ("edge triggered")
-                    blockType: BlockType.REPORTER,
-
-                    // label to display on the block
-                    text: 'My first block [MY_NUMBER] and [MY_STRING]',
-
-                    // true if this block should end a stack
-                    terminal: false,
-
-                    // where this block should be available for code - choose from:
-                    //   TargetType.SPRITE - for code in sprites
-                    //   TargetType.STAGE  - for code on the stage / backdrop
-                    // remove one of these if this block doesn't apply to both
-                    filter: [ TargetType.SPRITE, TargetType.STAGE ],
-
-                    // arguments used in the block
+                    opcode: 'playSound',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'load sound from [url]',
                     arguments: {
-                        MY_NUMBER: {
-                            // default value before the user sets something
-                            defaultValue: 123,
-
-                            // type/shape of the parameter - choose from:
-                            //     ArgumentType.ANGLE - numeric value with an angle picker
-                            //     ArgumentType.BOOLEAN - true/false value
-                            //     ArgumentType.COLOR - numeric value with a colour picker
-                            //     ArgumentType.NUMBER - numeric value
-                            //     ArgumentType.STRING - text value
-                            //     ArgumentType.NOTE - midi music value with a piano picker
-                            type: ArgumentType.NUMBER
-                        },
-                        MY_STRING: {
-                            // default value before the user sets something
-                            defaultValue: 'hello',
-
-                            // type/shape of the parameter - choose from:
-                            //     ArgumentType.ANGLE - numeric value with an angle picker
-                            //     ArgumentType.BOOLEAN - true/false value
-                            //     ArgumentType.COLOR - numeric value with a colour picker
-                            //     ArgumentType.NUMBER - numeric value
-                            //     ArgumentType.STRING - text value
-                            //     ArgumentType.NOTE - midi music value with a piano picker
-                            type: ArgumentType.STRING
+                        url: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'https://thenoceboeffect.github.io/sounds/a.ogg'
+                        }
+                    }
+                },
+                {
+                    opcode: 'setPitch',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'set pitch rate to [RATE]',
+                    arguments: {
+                        RATE: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: 1.0
                         }
                     }
                 }
-            ]
-        };
-    }
+            ],
+            menus: {},
 
-
-    /**
-     * implementation of the block with the opcode that matches this name
-     *  this will be called when the block is used
-     */
-    myFirstBlock ({ MY_NUMBER, MY_STRING }) {
-        // example implementation to return a string
-        return MY_STRING + ' : doubled would be ' + (MY_NUMBER * 2);
+            playSound({ URL }) {
+                loadSound(URL);
+            },
+            setPitch({ RATE }) {
+                setPitch(RATE);
+            },
+        }
     }
 }
 
-module.exports = Scratch3YourExtension;
+setPitch({ rate });
+if (player) {
+    player.playbackRate = rate; // Example: 1.0 is normal, 2.0 doubles pitch, 0.5 halves it
+}
+
+module.exports = Scratch3YourExtension; // Export the extension for unsandboxed usage
+
