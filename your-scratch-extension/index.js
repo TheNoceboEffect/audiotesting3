caf1266fdc9794f1c1ee715394d45169f6372805
has315 <hasllcdev@gmail.com>
2025-01-11 15:51:26 +0100

    fix syntax

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -9,16 +9,7 @@ class Scratch3YourExtension {
 
     constructor(runtime) {
         this.runtime = runtime;
-        this.player = new Tone.Player().toDestination();
-    }
-
-    //Define Helper Methods
-    playSound({ url }) {
-        this.player.load(url).then(() => {
-            this.player.start();
-        }).catch(error => {
-            console.error('Error loading sound:', error);
-        });
+        this.player = new Tone.Player();
     }
 
     /**
@@ -63,24 +54,82 @@ class Scratch3YourExtension {
                             defaultValue: 1.0
                         }
                     }
+                },
+                {
+                    // name of the function where your block code lives
+                    opcode: 'myFirstBlock',
+
+                    // type of block - choose from:
+                    //   BlockType.REPORTER - returns a value, like "direction"
+                    //   BlockType.BOOLEAN - same as REPORTER but returns a true/false value
+                    //   BlockType.COMMAND - a normal command block, like "move {} steps"
+                    //   BlockType.HAT - starts a stack if its value changes from false to true ("edge triggered")
+                    blockType: BlockType.REPORTER,
+
+                    // label to display on the block
+                    text: 'My first block [MY_NUMBER] and [MY_STRING]',
+
+                    // true if this block should end a stack
+                    terminal: false,
+
+                    // where this block should be available for code - choose from:
+                    //   TargetType.SPRITE - for code in sprites
+                    //   TargetType.STAGE  - for code on the stage / backdrop
+                    // remove one of these if this block doesn't apply to both
+                    filter: [ TargetType.SPRITE, TargetType.STAGE ],
+
+                    // arguments used in the block
+                    arguments: {
+                        MY_NUMBER: {
+                            // default value before the user sets something
+                            defaultValue: 123,
+
+                            // type/shape of the parameter - choose from:
+                            //     ArgumentType.ANGLE - numeric value with an angle picker
+                            //     ArgumentType.BOOLEAN - true/false value
+                            //     ArgumentType.COLOR - numeric value with a colour picker
+                            //     ArgumentType.NUMBER - numeric value
+                            //     ArgumentType.STRING - text value
+                            //     ArgumentType.NOTE - midi music value with a piano picker
+                            type: ArgumentType.NUMBER
+                        },
+                        MY_STRING: {
+                            // default value before the user sets something
+                            defaultValue: 'hello',
+
+                            // type/shape of the parameter - choose from:
+                            //     ArgumentType.ANGLE - numeric value with an angle picker
+                            //     ArgumentType.BOOLEAN - true/false value
+                            //     ArgumentType.COLOR - numeric value with a colour picker
+                            //     ArgumentType.NUMBER - numeric value
+                            //     ArgumentType.STRING - text value
+                            //     ArgumentType.NOTE - midi music value with a piano picker
+                            type: ArgumentType.STRING
+                        }
+                    }
                 }
             ],
             menus: {},
 
-            playSound({ URL }) {
-                loadSound(URL);
-            },
-            setPitch({ RATE }) {
-                setPitch(RATE);
-            },
+
         }
     }
+
+    playSound({ url }) {
+        this.player.load(url).then(() => {
+            this.player.start();
+        }).catch(error => {
+            console.error('Error loading sound:', error);
+        });
+    }
+
+    myFirstBlock ({ MY_NUMBER, MY_STRING }) {
+        // example implementation to return a string
+        return MY_STRING + ' : doubled would be ' + (MY_NUMBER * 2);
+    }
+
 }
 
-setPitch({ rate });
-if (player) {
-    player.playbackRate = rate; // Example: 1.0 is normal, 2.0 doubles pitch, 0.5 halves it
-}
 
 module.exports = Scratch3YourExtension; // Export the extension for unsandboxed usage
 
